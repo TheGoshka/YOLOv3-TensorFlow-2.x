49e63efdcdea787f27768cd4d6d52e115797a919
dvolchek <dvolchek@mail.ru>
2022-12-05 14:45:59 +0300

    Changed for DC task
    Changed for DC task

--- detection_demo.py
+++ detection_demo.py
@@ -5,13 +5,10 @@ import numpy as np
 import tensorflow as tf
 from yolov3.utils import detect_image, detect_realtime, detect_video, Load_Yolo_model, detect_video_realtime_mp
 from yolov3.configs import *
+import shutil
 
-image_path   = "image.jpg"
-video_path   = "test.mp4"
-
-yolo = Load_Yolo_model()
-detect_image(yolo, image_path, "detect.jpg", input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255,0,0))
-#detect_video(yolo, video_path, "", input_size=YOLO_INPUT_SIZE, show=False, rectangle_colors=(255,0,0))
-#detect_realtime(yolo, '', input_size=YOLO_INPUT_SIZE, show=True, rectangle_colors=(255, 0, 0))
-
-#detect_video_realtime_mp(video_path, "Output.mp4", input_size=YOLO_INPUT_SIZE, show=False, rectangle_colors=(255,0,0), realtime=False)
+def detect(image_path):
+  video_path   = "test.mp4"
+  yolo = Load_Yolo_model()
+  detect_image(yolo, image_path, "detect.jpg", input_size=YOLO_INPUT_SIZE, show=False, rectangle_colors=(255,0,0))
+  shutil.copyfile('/content/YOLOv3-TensorFlow-2.x/detect.jpg', '/content/detected.jpg')
